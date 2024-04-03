2931bf0e9f824dd226212f08eb66fca91f8934d0
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-04-03 16:39:23 +0200

    setup

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+/*
+import axios from "axios";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+export default {
+  components: {
+    Header,
+    Main,
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+   
+  },
+
+  mounted() {
+    
+  },
+};
+*/
+</script>
+
+<template>
+    
+    <Header />
+    <Main />
+    
+</template>
+
+<style scoped>
+
+</style>
